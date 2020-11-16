fa28479735eebb95c1abfab113a55b19a9cba3a7
Bogya Roland <roy.bogya@gmail.com>
2020-11-16 15:36:55 +0100

    Mappak kesz

---
+++ ff_WDatabse/ff_WDatabse/Models/Monster.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ff_WDatabse.Models
+{
+    public class Monster
+    {
+        [Key]
+        [StringLength(100)]
+        public string Name { get; set; }
+        public virtual Witcher Killedby { get; set; }
+
+        [Range(0, 10)]
+        public int Threat { get; set; }
+
+        [StringLength(100)]
+        public string Race { get; set; }
+
+        [Range(0, 10000)]
+        public int Bounty { get; set; }
+    }
+}
---
+++ ff_WDatabse/ff_WDatabse/Models/Witcher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ff_WDatabse.Models
+{
+    public class Witcher
+    {
+        [Key]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Range(0, 200)]
+        public int Age { get; set; }
+
+        [StringLength(100)]
+        public string School { get; set; }
+        [Range(0, 2000)]
+        public int AvaragePay { get; set; }
+    }
+}
