be5a86130de4340a39185d63f81a15109caa1e1c
Bogya Roland <roy.bogya@gmail.com>
2020-11-16 15:59:40 +0100

    Startup kesz

---
+++ ff_WDatabse/ff_WDatabse/Data/WDatabaseContext.cs
@@ -0,0 +1,23 @@
+﻿using ff_WDatabse.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace ff_WDatabse.Data
+{
+    public class WDatabaseContext : DbContext
+    {
+
+        public WDatabaseContext(DbContextOptions<WDatabaseContext> opt) : base(opt)
+        {
+            
+        }
+      
+        public DbSet<Human> Humen { get; set; }
+        public DbSet<Witcher> Witchers { get; set; }
+        public DbSet<Monster> Monsters { get; set; }
+    }
+}
