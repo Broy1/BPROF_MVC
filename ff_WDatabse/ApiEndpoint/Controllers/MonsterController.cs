95a8ece420bee78db8ea23a5b5f4e5ef686cb468
Bogya Roland <roy.bogya@gmail.com>
2021-06-14 15:29:13 +0200

    MonsterController added

--- ff_WDatabse/ApiEndpoint/Controllers/MonsterController.cs
+++ ff_WDatabse/ApiEndpoint/Controllers/MonsterController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Logic;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +10,40 @@ namespace ApiEndpoint.Controllers
 {
     public class MonsterController
     {
+        MonsterLogic logic;
+        public MonsterController(MonsterLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpDelete("{uid}")]
+        public void DeleteMonster(string uid)
+        {
+            logic.Delete(uid);
+        }
+
+        [HttpGet("{uid}")]
+        public Monster GetMonster(string uid)
+        {
+            return logic.Read(uid);
+        }
+
+        [HttpGet("{uid}")]
+        public IEnumerable<Monster> GetAllMonster()
+        {
+            return logic.GetAll();
+        }
+
+        [HttpPost]
+        public void AddMonster([FromBody] Monster item)
+        {
+            logic.Add(item);
+        }
+
+        [HttpPut("{oldid}")]
+        public void UpdateMonster(string oldid, [FromBody] Monster item)
+        {
+            logic.Update(oldid, item);
+        }
     }
 }
