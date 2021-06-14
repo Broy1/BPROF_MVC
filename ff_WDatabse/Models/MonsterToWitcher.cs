466c6d5acb6332bd249728eca26a961a63742e48
Bogya Roland <roy.bogya@gmail.com>
2021-06-14 18:24:04 +0200

    MonsterToWitcher class created

---
+++ ff_WDatabse/Models/MonsterToWitcher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class MonsterToWitcher
+    {
+        public string MonsterUid { get; set; }
+
+        public string WitcherUid { get; set; }
+    }
+}
