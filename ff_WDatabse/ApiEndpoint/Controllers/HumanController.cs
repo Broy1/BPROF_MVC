763c69382fefdfbb8bd42676d4a61bc84f5152eb
Bogya Roland <roy.bogya@gmail.com>
2021-06-14 15:29:06 +0200

    Human controller added

--- ff_WDatabse/ApiEndpoint/Controllers/HumanController.cs
+++ ff_WDatabse/ApiEndpoint/Controllers/HumanController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Logic;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +10,40 @@ namespace ApiEndpoint.Controllers
 {
     public class HumanController
     {
+        HumanLogic logic;
+        public HumanController(HumanLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpDelete("{uid}")]
+        public void DeleteHuman(string uid)
+        {
+            logic.Delete(uid);
+        }
+
+        [HttpGet("{uid}")]
+        public Human GetHuman(string uid)
+        {
+            return logic.Read(uid);
+        }
+
+        [HttpGet("{uid}")]
+        public IEnumerable<Human> GetAllHuman()
+        {
+            return logic.GetAll();
+        }
+
+        [HttpPost]
+        public void AddHuman([FromBody] Human item)
+        {
+            logic.Add(item);
+        }
+
+        [HttpPut("{oldid}")]
+        public void UpdateHuman(string oldid, [FromBody] Human item)
+        {
+            logic.Update(oldid, item);
+        }
     }
 }
