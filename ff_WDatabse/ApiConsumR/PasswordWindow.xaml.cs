06110ee2ac0050ad588571449e69e855eaedb652
Bogya Roland <roy.bogya@gmail.com>
2021-06-15 20:42:17 +0200

    Passwordwindow created

---
+++ ff_WDatabse/ApiConsumR/PasswordWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ApiConsumR
+{
+    /// <summary>
+    /// Interaction logic for PasswordWindow.xaml
+    /// </summary>
+    public partial class PasswordWindow : Window
+    {
+        public PasswordWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
